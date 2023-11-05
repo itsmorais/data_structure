598a219c27bdfee904a6dd8dabd6453b3b9d1494
Michael Morais <michael_morais@Outlook.com.br>
2023-11-05 19:47:04 -0300

    concluido P3

--- provaP3_mic/readme.md
+++ provaP3_mic/readme.md
@@ -7,5 +7,3 @@ P.3.6. Desenvolva um programa que leia um arquivo de texto e que armazene cada u
 palavras em um array (uma palavra por célula do array). A seguir, o programa efetua a
 ordenação usando o algoritmo *Merge Sort* e escreve, em um arquivo de saída (colocar o sufixo
 _ord.txt no nome do arquivo) as palavras em ordem lexicográfica.
-
-
--- provaP3_mic/src/index.ts
+++ provaP3_mic/src/index.ts
@@ -1,22 +1,33 @@
 import * as fs from 'fs';
 import { Merge } from '../merge_sort';
+
+// importando funções fornecidas pelo professor
 const merge_sort = new Merge
 
 // Ler arquivo
-const readFile = fs.readFileSync('./input_txt.txt', { encoding: 'utf8' })
+const readFile = fs.readFileSync('./input1.txt', { encoding: 'utf8' })
 
 
 // Armazena arquivo em array - separando cada palavra como um elemento do array
 const stringFile = readFile.split(' ')
 
 
+// Output do stringFile = array com palavras
+console.log("============ ARRAY COM DADOS DO ARQUIVO DE TEXTO! ============")
+console.log(stringFile)
+console.log("Quantidade de paralvras presentes no arquivo de entrada:", stringFile.length)
+
+
+
 
 // MERGE SORT NO ARRAY
 console.log("================= MERGE SORT =================")
+
 function sort_Array() {
-    const sorted_array = [merge_sort.sort(stringFile),'\n']
-    
-    fs.writeFileSync('./michael_ord.txt', JSON.stringify(sorted_array.toString()));
+    const sorted_array = [merge_sort.sort(stringFile)]
+    console.log(sorted_array[0])
+    console.log("Quantidade de palavras presentes no arquivo de saída:", sorted_array[0].length)
+    fs.writeFileSync('./michael_ord.txt', JSON.stringify(sorted_array[0].toString()));
 
 }
 
